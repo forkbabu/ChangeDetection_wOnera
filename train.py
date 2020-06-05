7e76402f4c7f0e2c8ca8ee1b23a241522a00cc0b
ucalyptus <santaclaus30011998@gmail.com>
2020-06-05 13:08:23 +0530

    try3

--- train.py
+++ train.py
@@ -94,6 +94,6 @@ history_save = pd.DataFrame(history.history).to_hdf(save_dir + history_name + "_
 
 # Save model and weights
 modelpath = save_dir + model_name + "_deconv.h5" 
-tf.compat.v1.keras.experimental.export_saved_model(model, modepath)
+tf.compat.v1.keras.experimental.export_saved_model(model, modelpath)
 #model.save_weights(modelpath,overwrite=True,save_format='h5')
 print('Trained model saved @ %s ' % save_dir)
